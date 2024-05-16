778798e0334a300639ad8ce60d4736184c98cc19
tfyguhw3 <marcusSocpsg@hotmail.com>
2024-05-16 22:43:15 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function search(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) right = mid - 1;
-      else left = mid + 1;
-    } else {
-      if (nums[mid] < target && target <= nums[right]) left = mid + 1;
-      else right = mid - 1;
-    }
+function findRedundantConnection(edges) {
+  const parent = new Array(edges.length + 1).fill(-1);
+  for (const edge of edges) {
+    const u = find(parent, edge[0]);
+    const v = find(parent, edge[1]);
+    if (u === v) return edge;
+    parent[u] = v;
   }
-  return -1;
+  return [];
+}
+function find(parent, i) {
+  if (parent[i] === -1) return i;
+  return find(parent, parent[i]);
 }
